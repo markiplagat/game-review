4201ecfd3ecdc12cc83cff0414a4a2b75bba26aa
markiplagat <markkiplagatkipruttoh@gmail.com>
2021-09-14 11:03:18 +0300

    Navigating through pages

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import {StyleSheet, View, Text} from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 
-const Home = () => {
+const Home = ({ navigation }) => {
+    const onPressHandler = () => {
+        navigation.navigate('ReviewDetails');
+        // Or use
+        // navigation.push('ReviewDetails');
+    }
     return(
         <View style={styles.container}>
             <Text style={styles.title}>Home page</Text>
+            <Button onPress={onPressHandler} title='Review Details' />
         </View>
     );
 };
